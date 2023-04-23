e50b489ae81f2c5fc45c840e5224abda88b8f834
Okabe-TO <ttt.sao.619@gmail.com>
2023-04-24 01:53:48 +0900

    メインコンテンツへの遷移ボタンを配置

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
-//import '*'
+import { Link } from 'react-router-dom'
+import styles from './Home.module.css'
 
 function Home() {
   return (
-    <div className="App">
-      <h1>HomePage.tsx</h1>
-      <p>ホームページのコンポーネントです。</p>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <section className={styles.navigation}>
+          <Link to="/create-book" className={styles.navItem}>
+            <img src="src/assets/unknown.png" alt="絵本生成" />
+            <h2>絵本生成</h2>
+          </Link>
+          <Link to="/rpg-book" className={styles.navItem}>
+            <img src="src/assets/unknown.png" alt="絵本RPG" />
+            <h2>絵本RPG</h2>
+          </Link>
+        </section>
+      </main>
     </div>
   )
 }
