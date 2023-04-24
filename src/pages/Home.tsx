513a4916f64638e7e96de5fa5846d43b0b374d70
Okabe-TO <ttt.sao.619@gmail.com>
2023-04-24 17:38:31 +0900

    コメントを追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,14 @@ function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        {/* ナビゲーションセクション */}
         <section className={styles.navigation}>
+          {/* 「絵本生成」へのリンク */}
           <Link to="/create-book" className={styles.navItem}>
             <img src="/assets/homeBookGeneration.png" alt="絵本生成" />
             <h2>絵本生成</h2>
           </Link>
+          {/* 「絵本RPG」へのリンク */}
           <Link to="/rpg-book" className={styles.navItem}>
             <img src="/assets/homeBookRPG.png" alt="絵本RPG" />
             <h2>絵本RPG</h2>
