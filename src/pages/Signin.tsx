772b389cbb747c53df07a1e2932b1e43e507563a
Kazuma Ito <zarigani2x@gmail.com>
2023-04-25 15:12:18 +0900

    サインインページのUIを更新

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -7,19 +7,28 @@ function Signin() {
       <div className={style.form_container}>
         <h1>サインイン</h1>
         <form className={style.form}>
-          <input type="mail" placeholder="メールアドレス" />
-          <input type="password" placeholder="パスワード" />
+          <div>
+            <div className={style.input_label}>メールアドレス</div>
+            <input type="mail" placeholder="xxxxx@example.com" />
+          </div>
+          <div>
+            <div className={style.input_label}>パスワード</div>
+            <input type="password" placeholder="password" />
+          </div>
+          <Link to="./forgot-password" className={style.notice}>
+            パスワードを忘れた？
+          </Link>
           <button type="submit" className={style.form_submit}>
             サインイン
           </button>
-          <Link to="./forget-password" className={style.notice}>
-            パスワードを忘れた？
-          </Link>
         </form>
         <hr />
-        <Link to="./signup" className={style.link_signup}>
-          サインアップ
-        </Link>
+        <div className={style.signup_nav_form}>
+          <div className={style.notice}>まだアカウントをお持ちでない方</div>
+          <Link to="./signup" className={style.link_signup}>
+            サインアップ
+          </Link>
+        </div>
       </div>
     </div>
   )
