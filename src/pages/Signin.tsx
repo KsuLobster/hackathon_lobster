2645261968bf4512fa117a9a70d687e0718fd6ec
Kazuma Ito <zarigani2x@gmail.com>
2023-04-25 12:59:13 +0900

    サインインページのUIを実装

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,10 +1,26 @@
-//import '*'
+import style from './Signin.module.css'
+import { Link } from 'react-router-dom'
 
 function Signin() {
   return (
-    <div className="App">
-      <h1>Signin.tsx</h1>
-      <p>サインインのコンポーネントです。</p>
+    <div className={style.app}>
+      <div className={style.form_container}>
+        <h1>サインイン</h1>
+        <form className={style.form}>
+          <input type="mail" placeholder="メールアドレス" />
+          <input type="password" placeholder="パスワード" />
+          <button type="submit" className={style.form_submit}>
+            サインイン
+          </button>
+          <Link to="./forget-password" className={style.notice}>
+            パスワードを忘れた？
+          </Link>
+        </form>
+        <hr />
+        <Link to="./signup" className={style.link_signup}>
+          サインアップ
+        </Link>
+      </div>
     </div>
   )
 }
