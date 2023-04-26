7485cf95685f371438ed8fb4e531e208d8063b3e
Okabe-TO <ttt.sao.619@gmail.com>
2023-04-26 14:05:26 +0900

    エンドポイントの隠蔽

--- src/pages/CreateBook/TestApiCall.tsx
+++ src/pages/CreateBook/TestApiCall.tsx
@@ -2,30 +2,36 @@
 import { useState } from 'react'
 
 function TestApiCall() {
+  // ストーリーのレスポンスを管理するためのステート
   const [response, setResponse] = useState<string | null>(null)
 
+  // APIを呼び出す非同期関数
   const callApi = async () => {
-    try {
-      const response = await fetch(
-        'https://us-central1-lobster-b93b4.cloudfunctions.net/generateStory/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            prompt: 'むかしむかし、あるところに...',
-          }),
-        }
-      )
+    // 環境変数からエンドポイントURLを取得
+    const apiUrl = process.env.REACT_APP_STORY_GENERATOR_API_URL
 
+    try {
+      // APIへのリクエストを送信
+      const response = await fetch(apiUrl as string, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt: 'むかしむかし、あるところに...',
+        }),
+      })
+
+      // レスポンスが正常でない場合、エラーをスロー
       if (!response.ok) {
         throw new Error('Error calling API.')
       }
 
+      // レスポンスのJSONを解析し、ストーリーをステートに設定
       const result = await response.json()
       setResponse(result.story)
     } catch (error) {
+      // API呼び出しでエラーが発生した場合、コンソールにエラーを出力し、ステートにエラーメッセージを設定
       console.error('Error calling API:', error)
       console.error('Full error object:', JSON.stringify(error))
       setResponse('Error calling API.')
@@ -34,7 +40,9 @@ function TestApiCall() {
 
   return (
     <div>
+      {/* API呼び出しをトリガーするボタン */}
       <button onClick={callApi}>Call API</button>
+      {/* レスポンスがある場合、その内容を表示 */}
       {response && <div>{response}</div>}
     </div>
   )
