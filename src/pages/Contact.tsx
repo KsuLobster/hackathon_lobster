ad31fcc7b700a4cbb156977acf6bec641d01823c
Kawasaki Yuichi <yuyux0910@gmail.com>
2023-04-27 11:32:54 +0900

    メールアドレスの入力欄作成とコンポーネントの中央寄せ

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,14 @@
 //import '*'
 import { ChangeEvent, useState } from 'react'
 import {
+  Container,
   FormControl,
   FormLabel,
   FormControlLabel,
   RadioGroup,
   Radio,
+  TextField,
 } from '@mui/material'
-import { TextareaAutosize } from '@mui/material'
 
 function Contact() {
   const [value, setValue] = useState('')
@@ -19,46 +20,62 @@ function Contact() {
   return (
     <div className="App">
       <p>お問い合わせページのコンポーネントです。</p>
-      <FormControl>
-        <FormLabel id="demo-radio-buttons-group-label">
-          当てはまるものを選択してください
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="contact-radio-buttons-group-label"
-          defaultValue="request"
-          name="radio-buttons-group"
-          value={value}
-          onChange={handleChange}
-        >
-          <FormControlLabel
-            value="question"
-            control={<Radio />}
-            label="ご質問"
-          />
-          <FormControlLabel
-            value="suggestion"
-            control={<Radio />}
-            label="ご提案"
-          />
-          <FormControlLabel value="other" control={<Radio />} label="その他" />
-        </RadioGroup>
-      </FormControl>
-      {value === 'question' && (
-        <div>
-          <TextareaAutosize minRows={3} placeholder="question" />
-        </div>
-      )}
-      {value === 'suggestion' && (
-        <div>
-          <TextareaAutosize minRows={3} placeholder="suggestion" />
-        </div>
-      )}
-      {value === 'other' && (
-        <div>
-          <TextareaAutosize minRows={3} placeholder="other" />
-        </div>
-      )}
+      <Container maxWidth="xs">
+        <FormControl>
+          <FormLabel id="radio-buttons-group-label">
+            当てはまるものを選択してください
+          </FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="contact-radio-buttons-group-label"
+            defaultValue="request"
+            name="radio-buttons-group"
+            value={value}
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="question"
+              control={<Radio />}
+              label="ご質問"
+            />
+            <FormControlLabel
+              value="suggestion"
+              control={<Radio />}
+              label="ご提案"
+            />
+            <FormControlLabel
+              value="other"
+              control={<Radio />}
+              label="その他"
+            />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="mailadress-input-textarea-label">
+            メールアドレスをご入力ください
+          </FormLabel>
+          <TextField placeholder="youradress@xxx" />
+          {value === 'question' && (
+            <div>
+              <TextField
+                minRows={3}
+                placeholder="question"
+                variant="outlined"
+              />
+            </div>
+          )}
+          {value === 'suggestion' && (
+            <div>
+              <TextField minRows={3} placeholder="suggestion" />
+            </div>
+          )}
+          {value === 'other' && (
+            <div>
+              <TextField minRows={3} placeholder="other" />
+            </div>
+          )}
+        </FormControl>
+      </Container>
     </div>
   )
 }
