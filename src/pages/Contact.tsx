11a38364b65bddc45a7e54700c2a968cf629a47d
Kawasaki Yuichi <yuyux0910@gmail.com>
2023-04-27 17:19:34 +0900

    メールアドレスの入力欄と送信ボタンの実装

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -49,11 +49,6 @@ function Contact() {
     console.log(`submit: ${data.content}`)
     console.log(`submit: ${data.adress}`)
   }
-  // const [value, setValue] = useState('')
-  //
-  // const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   setValue((event.target as HTMLInputElement).value)
-  // }
 
   return (
     <div className="App">
@@ -81,79 +76,44 @@ function Contact() {
                   {...field}
                   value={1}
                   control={<Radio />}
-                  label="質問"
+                  label="ご質問"
                 />
                 <FormControlLabel
                   {...field}
                   value={2}
                   control={<Radio />}
-                  label="提案"
+                  label="ご意見・提案"
+                />
+                <FormControlLabel
+                  {...field}
+                  value={0}
+                  control={<Radio />}
+                  label="その他"
                 />
               </RadioGroup>
               <FormHelperText>{fieldState.error?.message}</FormHelperText>
             </FormControl>
           )}
         />
+        <Controller
+          name="adress"
+          control={control}
+          rules={validationRules.adress}
+          render={({ field, fieldState }) => (
+            <TextField
+              {...field}
+              type="text"
+              label="メールアドレス"
+              error={fieldState.invalid}
+              helperText={fieldState.error?.message}
+            />
+          )}
+        />
+
         <Button variant="contained" type="submit">
           送信する
         </Button>
       </Stack>
-      {/*<Container maxWidth="xs">*/}
-      {/*  <FormControl>*/}
-      {/*    <FormLabel id="radio-buttons-group-label">*/}
-      {/*      当てはまるものを選択してください*/}
-      {/*    </FormLabel>*/}
-      {/*    <RadioGroup*/}
-      {/*      row*/}
-      {/*      aria-labelledby="contact-radio-buttons-group-label"*/}
-      {/*      defaultValue="request"*/}
-      {/*      name="radio-buttons-group"*/}
-      {/*      value={value}*/}
-      {/*      onChange={handleChange}*/}
-      {/*    >*/}
-      {/*      <FormControlLabel*/}
-      {/*        value="question"*/}
-      {/*        control={<Radio />}*/}
-      {/*        label="ご質問"*/}
-      {/*      />*/}
-      {/*      <FormControlLabel*/}
-      {/*        value="suggestion"*/}
-      {/*        control={<Radio />}*/}
-      {/*        label="ご提案"*/}
-      {/*      />*/}
-      {/*      <FormControlLabel*/}
-      {/*        value="other"*/}
-      {/*        control={<Radio />}*/}
-      {/*        label="その他"*/}
-      {/*      />*/}
-      {/*    </RadioGroup>*/}
-      {/*  </FormControl>*/}
-      {/*  <FormControl>*/}
-      {/*    <FormLabel id="mailadress-input-textarea-label">*/}
-      {/*      メールアドレスをご入力ください*/}
-      {/*    </FormLabel>*/}
-      {/*    <TextField placeholder="youradress@xxx" />*/}
-      {/*    {value === 'question' && (*/}
-      {/*      <div>*/}
-      {/*        <TextField*/}
-      {/*          minRows={3}*/}
-      {/*          placeholder="question"*/}
-      {/*          variant="outlined"*/}
-      {/*        />*/}
-      {/*      </div>*/}
-      {/*    )}*/}
-      {/*    {value === 'suggestion' && (*/}
-      {/*      <div>*/}
-      {/*        <TextField minRows={3} placeholder="suggestion" />*/}
-      {/*      </div>*/}
-      {/*    )}*/}
-      {/*    {value === 'other' && (*/}
-      {/*      <div>*/}
-      {/*        <TextField minRows={3} placeholder="other" />*/}
-      {/*      </div>*/}
-      {/*    )}*/}
-      {/*  </FormControl>*/}
-      {/*</Container>*/}
     </div>
   )
 }
