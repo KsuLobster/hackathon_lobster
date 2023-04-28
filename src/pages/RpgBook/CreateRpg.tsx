b811d8413f96aad412cfbac03aaa89f86293320f
masa31y <masaya.yoshikawa.31@icloud.com>
2023-04-28 20:19:12 +0900

    CreateRpgを作成

---
+++ src/pages/RpgBook/CreateRpg.tsx
@@ -0,0 +1,51 @@
+// RpgBook/CreateRpg.tsx
+import { useState } from 'react'
+
+function CreateRpg() {
+  // ストーリーのレスポンスを管理するためのステート
+  const [response, setResponse] = useState<string | null>(null)
+
+  // APIを呼び出す非同期関数
+  const callApi = async () => {
+    // 環境変数からエンドポイントURLを取得
+    const apiUrl = process.env.REACT_APP_STORY_GENERATOR_API_URL
+
+    try {
+      // APIへのリクエストを送信
+      const response = await fetch(apiUrl as string, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt: 'むかしむかし、あるところに...',
+        }),
+      })
+
+      // レスポンスが正常でない場合、エラーをスロー
+      if (!response.ok) {
+        throw new Error('Error calling API.')
+      }
+
+      // レスポンスのJSONを解析し、ストーリーをステートに設定
+      const result = await response.json()
+      setResponse(result.story)
+    } catch (error) {
+      // API呼び出しでエラーが発生した場合、コンソールにエラーを出力し、ステートにエラーメッセージを設定
+      console.error('Error calling API:', error)
+      console.error('Full error object:', JSON.stringify(error))
+      setResponse('Error calling API.')
+    }
+  }
+
+  return (
+    <div>
+      {/* API呼び出しをトリガーするボタン */}
+      <button onClick={callApi}>Call API</button>
+      {/* レスポンスがある場合、その内容を表示 */}
+      {response && <div>{response}</div>}
+    </div>
+  )
+}
+
+export default CreateRpg
--- src/pages/RpgBook/RpgBook.tsx
+++ src/pages/RpgBook/RpgBook.tsx
@@ -1,9 +1,10 @@
-//import '*'
+import CreateRpg from './CreateRpg'
 
 function RpgBook() {
   return (
     <div className="App">
       <h1>RpgBook.tsx</h1>
+      <CreateRpg />
       <p>絵本RPGページのコンポーネントです。</p>
     </div>
   )
