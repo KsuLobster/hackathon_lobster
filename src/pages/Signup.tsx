107c29365713adb393cb50b6bfecf44a994f577a
Ryota <studyprograming51@gmail.com>
2023-04-29 13:51:34 +0900

    singup修正

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom' // ここにナビゲーション
 import 'firebase/auth'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebase'
-import { useNavigate } from 'react-router-dom'
 
 const Signup = (): JSX.Element => {
   const [email, setEmail] = useState<string>('')
